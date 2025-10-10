bf0ee7f461cbbfda8736738617fe5cca63df0e0b
cessie840 <maniclangprincessjenin40@gmail.com>
2025-10-11 02:40:49 +0800

    use hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "leaflet/dist/leaflet.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
